20a1e3eeefd17d37c8af183e19cea9c7054bb0d7
황미애 <freemiae99@gmail.com>
2022-05-12 17:05:59 +0900

    다시해보기

--- common.js
+++ common.js
@@ -3,54 +3,73 @@ $(function(){
 });
 
 //tab
-
+//왜 동작은 다되는데 같이먹는거냐 on같이나옴 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
 var tabUI = function(){
-    //wai-aria 부분
-    var $tab = $('.ul-tab'); 
+    //wai-aria html에 나오게 기본셋팅
+    var $tab = $('.ul-tab');     
+    $tab.find('li.on').find('>a').attr({'title':'선택됨','aria-selected':'true'});
+    $tab.find('li.on').siblings().find('>a').attr('aria-selected','false').removeAttr('title');
 
-    if($tab.length > 0){
-        $tab.find('li.on').find('>a').attr({'title':'선택됨','aria-selected':'true'}).siblings('li').find('>a').attr('aria-selected','false').removeAttr('title');
-    }
-    
+   
 
-    $(document).on('click','.ul-tab a',function(e){
-        e.preventDefault;
-        var $this = $(this),
-            idx = $this.parent('li').index();
-            $this.parent().addClass('on').siblings('li').removeClass('on')
-            $this.parents('.ul-tab').find('.tab_panel').eq(idx).addClass('on').siblings('.tab_panel').removeClass('on');
-    }); 
+     //tab 초기화 (의미를 모름 ㅜㅜ 으어 )
+     const tabInit = function() {
+        $('.ul-tab').each(function() {
+            const firstText = $(this).find('.ul-tab li:first-child a').text();
 
-
-    //tab 초기화
-    const tabInit = function() {
-        $('.scroll_area').each(function() {
-            const firstText = $(this).find('.scroll_list li:first-child a').text();
-
-            $(this).find('.scroll_list li:first-child').addClass('on')
+            $(this).find('.ul-tab li:first-child').addClass('on')
                 .children('a').attr({'aria-selected': true, 'title': '선택됨'})
                 .parent('li').siblings('li').children('a').attr({'aria-selected': false})
-                .parents('.scroll_area').find('.list_wrap:eq(0)').text(firstText).addClass('on');
+                .parents('.ul-tab').find('.tab_panel:eq(0)').text(firstText).addClass('on');
         });
     }
     tabInit();
-
-    $(document).on('click','.scroll_list a', function(e){
-        e.preventDefault();
+ 
+    //기본탭
+    $(document).on('click','.ul-tab a',function(e){
+        e.preventDefault;
         var $this = $(this);
         var idx = $this.parent('li').index();
-        var $scrollWrap = $this.parents('.scroll_list');
-        var positionLeft = $this.parent('li').position().left + Number($this.parent('li').css('marginLeft').replace(/[^0-9]/g, ''));
-        var scrollLeft = $scrollWrap.scrollLeft();
+
+        //소스를 합치고싶다 scroll로 분기태워서 ㅜㅜㅜㅜㅜㅜ
 
         $this.attr({'aria-selected': true, 'title': "선택됨"})
-          .parent('li').addClass('on')
-          .siblings('li').removeClass('on')
-          .children('a').attr({'aria-selected': false}).removeAttr('title')
-          .parents('.scroll_area').find('.list_wrap').eq(idx).addClass('on').siblings('.list_wrap').removeClass('on');
+        .parent('li').addClass('on')
+        .siblings('li').removeClass('on')
+        .children('a').attr({'aria-selected': false}).removeAttr('title')
+        .parents('.ul-tab').find('.tab_panel').eq(idx).addClass('on').siblings('.tab_panel').removeClass('on');
+        
+    }); 
 
-        $scrollWrap.animate({'scrollLeft': positionLeft + scrollLeft}, 150);
+
+
+    //혬1004ver
+ 
+    // $(document).on('click','.scroll_list a', function(e){
+    //     e.preventDefault();
+    //     var $this = $(this);
+    //     var idx = $this.parent('li').index();
+    //     var $scrollWrap = $this.parents('.scroll_list');
+    //     var positionLeft = $this.parent('li').position().left + Number($this.parent('li').css('marginLeft').replace(/[^0-9]/g, ''));
+    //     var scrollLeft = $scrollWrap.scrollLeft();
+
+    //     $this.attr({'aria-selected': true, 'title': "선택됨"})
+    //       .parent('li').addClass('on')
+    //       .siblings('li').removeClass('on')
+    //       .children('a').attr({'aria-selected': false}).removeAttr('title')
+    //       .parents('.ul-tab').find('.tab_panel').eq(idx).addClass('on').siblings('.tab_panel').removeClass('on');
+
+    //     $scrollWrap.animate({'scrollLeft': positionLeft + scrollLeft}, 150);
+    // }) 
+
+     $(document).on('click','.scroll_list a', function(e){
+        e.preventDefault();
+        var $this = $(this);
+        var $scrollWrap = $this.parents('.scroll_list');
+
+        $scrollWrap.animate({'scrollLeft': $this.parent('li').position().left}, 150);
+        console.log($this.parent('li').position().left)
+
     }) 
 
-    
 }
